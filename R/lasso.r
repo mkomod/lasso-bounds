75369277ccb94c14dd8c1537a470d22adec017a1
mkomod <mkomodromos01@gmail.com>
2020-11-13 18:15:50 +0000

    init: python base

--- R/lasso.r
+++ R/lasso.r
@@ -12,15 +12,15 @@ gen_data <- function(n, p, s, sig, rho) {
     norms <- apply(X, 2, function(x) norm(as.matrix(x), type="F"))
     X <- t(apply(X, 1, function(x) x / norms)) * sqrt(n)
 
-    e <- rnorm(n)
+    e <- rnorm(n, 0, sd=sig)
     beta.0 <- c(rep(1, s), rep(0, p-s))
-    Y <- X %*% beta.0 + sig * e
+    Y <- X %*% beta.0 + e
 
     return(list(Y=Y, X=X, beta=beta.0, rho=rho, sig=sig, p=p, n=n, s=s))
 }
 
 
-N_exp <- 1e3
+N_exp <- 0.5e3
 N.lambda <- 100
 lambdas <- seq(0.0001, 10.001, length.out=N.lambda)
 prediction.error <- matrix(nrow=N_exp, ncol=length(lambdas))
@@ -30,22 +30,19 @@ for (i in 1:N_exp) {
     l <- glmnet::glmnet(d$X, d$Y, 
 	    alpha=1,                          # for LASSO
 	    # nlambda=100,
-	    standardize=F,
+	    standardize=FALSE,
+	    family="gaussian",
 	    thresh=1e-9,
 	    maxit=1e6,
 	    lambda=lambdas,
 	    intercept=FALSE)
     
     for (j in seq_along(lambdas)) {
-	prediction.error[i, j] <- 
-	    sum((d$X %*% (l$beta[ , j] - d$beta))^2)
+	prediction.error[i, j] <- sum((d$X %*% (l$beta[ , j] - d$beta))^2)
     }
 }
 
-
-plot(lambdas, rev(apply(prediction.error, 2, mean)), type="l")
-lines(lambdas, rev(apply(prediction.error, 2, function(p) 
-			 quantile(p, 0.025))))
-lines(lambdas, rev(apply(prediction.error, 2, function(p) 
-			 quantile(p, 0.975))))
+plot(lambdas, rev(apply(prediction.error, 2, mean)), type="l", ylim=c(0, 100))
+lines(lambdas, rev(apply(prediction.error, 2, function(p) quantile(p, 0.025))))
+lines(lambdas, rev(apply(prediction.error, 2, function(p) quantile(p, 0.975))))
 
