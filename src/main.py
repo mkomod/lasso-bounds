c26b337190e5db55b8e6581f21c98eeb320769bb
Llamrei <al3615@ic.ac.uk>
2020-11-14 13:07:40 +0000

    Add formating to figures and CLI options

--- src/main.py
+++ src/main.py
@@ -1,34 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+from datetime import datetime
 
 from sklearn import linear_model
 
+
 def normalise_matrix(X):
-    col_means = np.mean(X, axis = 0)
+    col_means = np.mean(X, axis=0)
     X = X - col_means
 
-    col_norm = np.linalg.norm(X, axis = 0)
+    col_norm = np.linalg.norm(X, axis=0)
     X = X / col_norm
 
     X = X * np.sqrt(X.shape[0])
 
     return X
 
+
 def generate_data(n, p, s, sig, rho, eta):
     """
     Generates data according to algorithm 1 from
     """
-    beta_01 = np.concatenate((np.ones(s), np.zeros(p-s)))
-    beta_02 = np.concatenate((np.ones(s), np.zeros(p**2-s)))
+    beta_01 = np.concatenate((np.ones(s), np.zeros(p - s)))
+    beta_02 = np.concatenate((np.ones(s), np.zeros(p ** 2 - s)))
 
     mean = np.zeros(p)
     cov = np.diag(np.ones(p) - rho) + rho
     X = np.random.multivariate_normal(mean, cov, n)
     X2 = X.copy()
 
-    for j in range(0, p-1):
+    for j in range(0, p - 1):
         N = np.random.multivariate_normal(mean, np.eye(p), n)
-        X2 = np.concatenate((X2, X+eta*N), axis = 1)
+        X2 = np.concatenate((X2, X + eta * N), axis=1)
 
     epsilon = np.random.normal(0, sig, n)
     Y = X.dot(beta_01) + epsilon
@@ -36,70 +40,111 @@ def generate_data(n, p, s, sig, rho, eta):
     X = normalise_matrix(X)
     X2 = normalise_matrix(X2)
 
-    return {"X": X, "X2": X2, "Y": Y, "beta_01": beta_01, "beta_02": beta_02,
-            "n": n, "p": p, "s": s, "sig":sig, "rho": rho}
+    return {
+        "X": X,
+        "X2": X2,
+        "Y": Y,
+        "beta_01": beta_01,
+        "beta_02": beta_02,
+        "n": n,
+        "p": p,
+        "s": s,
+        "sig": sig,
+        "rho": rho,
+    }
 
 
 def fit_lasso(X, y, lam):
     """
     Fits LASSO with LARS
     """
-    model = linear_model.LassoLars(alpha = lam, fit_intercept = False, 
-            normalize = True)
+    model = linear_model.LassoLars(alpha=lam, fit_intercept=False, normalize=True)
     model.fit(X, y)
     beta_hat = model.coef_
     return beta_hat
-    
 
-n, p, s, sig, rho, eta = 20, 40, 4, 1, 0, 0.001
 
-Nexp = 100
-lambdas = np.linspace(0.001, 0.7, 100)
-prediction_error = np.zeros((len(lambdas), Nexp))
-prediction_error2 = np.zeros((len(lambdas), Nexp))
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        try:
+            n, p, s, sig, rho, eta, Nexp = sys.argv[1:]
+        except ValueError:
+            raise (
+                ValueError(
+                    "You did not supply all experiment values\n Need n, p, s, sigma, rho, eta, number_of_experiments"
+                )
+            )
+    else:
+        n, p, s, sig, rho, eta = 20, 40, 4, 1, 0, 0.1
+        Nexp = 5
 
-print("starting", end = "")
+    lambdas = np.linspace(0.001, 0.7, 100)
+    prediction_error = np.zeros((len(lambdas), Nexp))
+    prediction_error2 = np.zeros((len(lambdas), Nexp))
 
-for exp_num in range(Nexp):
-    data = generate_data(n, p, s, sig, rho, eta)
-    beta_01 = data.get("beta_01")
-    beta_02 = data.get("beta_02")
-    X = data.get("X")
-    X2 = data.get("X2")
-    Y = data.get("Y")
+    print("starting", end="")
 
-    for i, lam in enumerate(lambdas):
-        beta_hat = fit_lasso(X, Y, lam)
-        beta_hat2 = fit_lasso(X2, Y, lam)
-        prediction_error[i, exp_num] = np.sum(np.power(np.dot(X, beta_hat - beta_01), 2))
-        prediction_error2[i, exp_num] = np.sum(np.power(np.dot(X2, beta_hat2 - beta_02), 2))
-    
-    print(f"\r experiment {exp_num+1} / {Nexp}", end = "", flush = True)
+    for exp_num in range(Nexp):
+        data = generate_data(n, p, s, sig, rho, eta)
+        beta_01 = data.get("beta_01")
+        beta_02 = data.get("beta_02")
+        X = data.get("X")
+        X2 = data.get("X2")
+        Y = data.get("Y")
 
-print("\nDone")
+        for i, lam in enumerate(lambdas):
+            beta_hat = fit_lasso(X, Y, lam)
+            beta_hat2 = fit_lasso(X2, Y, lam)
+            prediction_error[i, exp_num] = np.sum(
+                np.power(np.dot(X, beta_hat - beta_01), 2)
+            )
+            prediction_error2[i, exp_num] = np.sum(
+                np.power(np.dot(X2, beta_hat2 - beta_02), 2)
+            )
 
-# mean prediction error 
-mean_pde = np.mean(prediction_error, axis = 1)
-mean_pde2 = np.mean(prediction_error2, axis = 1)
+        print(f"\r experiment {exp_num+1} / {Nexp}", end="", flush=True)
 
-lambda_min_1 = lambdas[np.argmin(mean_pde)]
-lambda_min_2 = lambdas[np.argmin(mean_pde2)]
+    print("\nDone")
 
-# ALGO 1
-plt.plot(lambdas, mean_pde, label = "Algo 1", color = "b")
-plt.fill_between(lambdas, np.quantile(prediction_error, 0.025, axis = 1), np.quantile(prediction_error, 0.975, axis = 1),
-color = "b", alpha = .2)
-plt.axvline(lambda_min_1, color = "b")
+    # mean prediction error
+    mean_pde = np.mean(prediction_error, axis=1)
+    mean_pde2 = np.mean(prediction_error2, axis=1)
 
-# ALGO 2
-plt.plot(lambdas, mean_pde2, label = "Algo 2", color = "r")
-plt.fill_between(lambdas, np.quantile(prediction_error2, 0.025, axis = 1), np.quantile(prediction_error2, 0.975, axis = 1),
-color = "r", alpha = .2)
-plt.axvline(lambda_min_2, color = "r")
+    lambda_min_1 = lambdas[np.argmin(mean_pde)]
+    lambda_min_2 = lambdas[np.argmin(mean_pde2)]
 
-plt.title(f"Parameters: {(n, p, s, sig, rho, eta)}")
-plt.axvline(np.sqrt(2)*lambda_min_1, c = "k", ls = "--")
-plt.xlabel("$\lambda$")
-plt.ylabel("Prediction Error")
-plt.legend()
-plt.show()
+    # ALGO 1
+    plt.plot(lambdas, mean_pde, label="Algo 1", color="b")
+    plt.fill_between(
+        lambdas,
+        np.quantile(prediction_error, 0.025, axis=1),
+        np.quantile(prediction_error, 0.975, axis=1),
+        color="b",
+        alpha=0.2,
+    )
+    plt.axvline(lambda_min_1, color="b")
+
+    # ALGO 2
+    plt.plot(lambdas, mean_pde2, label="Algo 2", color="r")
+    plt.fill_between(
+        lambdas,
+        np.quantile(prediction_error2, 0.025, axis=1),
+        np.quantile(prediction_error2, 0.975, axis=1),
+        color="r",
+        alpha=0.2,
+    )
+    plt.axvline(lambda_min_2, color="r")
+
+    plt.title(
+        r"Parameters: n={}, p={}, s={}, $\sigma$={}, $\rho$={}, $\eta$={}".format(
+            n, p, s, sig, rho, eta
+        )
+    )
+    plt.axvline(np.sqrt(2) * lambda_min_1, c="k", ls="--")
+    plt.xlabel("$\lambda$")
+    plt.ylabel("Prediction Error")
+    plt.legend()
+    plt.show()
+    plt.savefig(
+        f"figs/numexp{Nexp}_n{n:}p{p}s{s}sig{sig}rho{rho}eta{str(eta).replace('.','_')}"
+    )
