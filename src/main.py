83411484bc59cfaa2c7c19dcdcedb36fd08d0581
Jose Pablo Folch <josepablofolch@Joses-MacBook-Pro.local>
2020-11-16 13:55:43 +0000

    Fixed assumption error

--- src/main.py
+++ src/main.py
@@ -51,12 +51,16 @@ def fit_lasso(X, y, lam):
     return beta_hat
     
 
-n, p, s, sig, rho, eta = 20, 40, 4, 1, 0, 0.001
+n, p, s, sig, rho, eta = 20, 40, 4, 1, 0.99, 20
 
-Nexp = 100
+Nexp = 25
 lambdas = np.linspace(0.001, 0.7, 100)
 prediction_error = np.zeros((len(lambdas), Nexp))
 prediction_error2 = np.zeros((len(lambdas), Nexp))
+
+min_lambdas = np.zeros(Nexp)
+min_lambdas2 = np.zeros(Nexp)
+min_PEs = np.zeros(Nexp)
 
 print("starting", end = "")
 
@@ -71,10 +75,22 @@ for exp_num in range(Nexp):
     for i, lam in enumerate(lambdas):
         beta_hat = fit_lasso(X, Y, lam)
         beta_hat2 = fit_lasso(X2, Y, lam)
+        
         prediction_error[i, exp_num] = np.sum(np.power(np.dot(X, beta_hat - beta_01), 2))
         prediction_error2[i, exp_num] = np.sum(np.power(np.dot(X2, beta_hat2 - beta_02), 2))
     
     print(f"\r experiment {exp_num+1} / {Nexp}", end = "", flush = True)
+
+    # ignore this, this is for my experiment
+    pe_min = np.min(prediction_error[:, exp_num])
+    min_PEs[exp_num] = pe_min
+
+    # find the minimum lambda for this particular experiment
+    lambda_min = lambdas[np.argmin(prediction_error[:, exp_num])]
+    lambda_min2 = lambdas[np.argmin(prediction_error2[:, exp_num])]
+
+    min_lambdas[exp_num] = lambda_min
+    min_lambdas2[exp_num] = lambda_min2
 
 print("\nDone")
 
@@ -82,24 +98,35 @@ print("\nDone")
 mean_pde = np.mean(prediction_error, axis = 1)
 mean_pde2 = np.mean(prediction_error2, axis = 1)
 
-lambda_min_1 = lambdas[np.argmin(mean_pde)]
-lambda_min_2 = lambdas[np.argmin(mean_pde2)]
+lambda_min_1 = np.mean(min_lambdas)
+lambda_min_2 = np.mean(min_lambdas2)
 
-# ALGO 1
-plt.plot(lambdas, mean_pde, label = "Algo 1", color = "b")
-plt.fill_between(lambdas, np.quantile(prediction_error, 0.025, axis = 1), np.quantile(prediction_error, 0.975, axis = 1),
-color = "b", alpha = .2)
-plt.axvline(lambda_min_1, color = "b")
+#ignore this, it's for my experiment
+#print()
+#print(f"Average Minimum Lambda: {np.mean(min_lambdas)}")
+#print(f"Standard Deviation: {np.std(min_lambdas)}")
+#print()
+#print(f"Average Minimum Prediction Error: {np.mean(min_PEs)}")
+#print(f"Standard Deviation: {np.std(min_PEs)}")
 
-# ALGO 2
-plt.plot(lambdas, mean_pde2, label = "Algo 2", color = "r")
-plt.fill_between(lambdas, np.quantile(prediction_error2, 0.025, axis = 1), np.quantile(prediction_error2, 0.975, axis = 1),
-color = "r", alpha = .2)
-plt.axvline(lambda_min_2, color = "r")
+plot_graphs = True
 
-plt.title(f"Parameters: {(n, p, s, sig, rho, eta)}")
-plt.axvline(np.sqrt(2)*lambda_min_1, c = "k", ls = "--")
-plt.xlabel("$\lambda$")
-plt.ylabel("Prediction Error")
-plt.legend()
-plt.show()
+if plot_graphs == True:
+    # ALGO 1
+    plt.plot(lambdas, mean_pde, label = "Algo 1", color = "b")
+    #plt.fill_between(lambdas, np.quantile(prediction_error, 0.025, axis = 1), np.quantile(prediction_error, 0.975, axis = 1),
+    #color = "b", alpha = .2)
+    plt.axvline(lambda_min_1, color = "b")
+
+    # ALGO 2
+    plt.plot(lambdas, mean_pde2, label = "Algo 2", color = "r")
+    #plt.fill_between(lambdas, np.quantile(prediction_error2, 0.025, axis = 1), np.quantile(prediction_error2, 0.975, axis = 1),
+    #color = "r", alpha = .2)
+    plt.axvline(lambda_min_2, color = "r")
+
+    plt.title(f"Parameters: {(n, p, s, sig, rho, eta)}")
+    plt.axvline(np.sqrt(2)*lambda_min_1, c = "k", ls = "--")
+    plt.xlabel("$\lambda$")
+    plt.ylabel("Prediction Error")
+    plt.legend()
+    plt.show()
